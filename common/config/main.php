b561cf78cacef4d0e8c2cd3fa2940d36abd07549
idontwindows <eduzaragozajr@gmial.com>
2020-12-23 14:17:14 +0800

    update main

--- common/config/main.php
+++ common/config/main.php
@@ -14,7 +14,7 @@ return [
         'gii' => [
             'class' => 'yii\gii\Module',
             //'allowedIPs' => ['127.0.0.1', '::1', '192.168.1.*'] // adjust this to your needs
-            'allowedIPs' => ['*'] // adjust this to your needs
+            //'allowedIPs' => ['*'] // adjust this to your needs
         ],
         'message' => [
             'class' => 'thyseus\message\Module',
